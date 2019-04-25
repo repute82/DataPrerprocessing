2aff8632fa9aa24cf0ff97d83214999a7f4a2c0b
Jang Won Bae <jwbae@etri.re.kr>
2019-04-25 23:26:18 +0900

    modifying groupby

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -14,8 +14,8 @@ import pandas as pd
 
 if __name__ == "__main__": 
     # readCSVfile("KT_CZ_Analysis.card_by_upjong.csv")
-    df = pd.read_csv("KT_CZ_Analysis.card_by_upjong.csv", skiprows=[0], names=['일련번호','행정동 코드', '년', '월', '업종코드', '결제금액', '결제고객', '결제건수', '대분류', '생활업종별'])
-    print(type(df))
-    df.groupby(['행정동 코드'], as_index=False).mean()
-    print(df.describe())
+    df = pd.read_csv("KT_CZ_Analysis.card_by_upjong.csv")
+    print(df.columns)
+    group1= df.revenue.groupby(['행정동 코드', '년', '월', '업종코드'])    
+    #print(group1.mean())
 
