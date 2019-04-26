4ace208945a9ef7b1bd8a63a2f223b75126e53f1
Jang Won Bae <jwbae@etri.re.kr>
2019-04-26 16:37:22 +0900

    modyfying living_ingu && processing

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -22,15 +22,14 @@ if __name__ == "__main__":
         # readCSVfile
         card_by_upjong = pd.read_csv("KT_CZ_Analysis.card_by_upjong.csv")
         # removing upjong_code = 3002, which is not in other tables
-        card_by_upjong = card_by_upjong[card_by_upjong.upjong_code != 3002]
+        # card_by_upjong = card_by_upjong[card_by_upjong.upjong_code != 3002]
         #grouped by year,month,hd_cd,upjong_code
         group1= card_by_upjong.groupby([card_by_upjong.year, card_by_upjong.month, card_by_upjong.hd_cd, card_by_upjong.upjong_code])            
 
         # make dataframe as result
-        result = group1['revenue','customer_count','payment_count'].sum()  
+        result_1 = group1['revenue','customer_count','payment_count'].sum()  
 
-        #end: writing CSV file    
-        result.to_csv('result.csv', index=True,header = True)
+      
         print("End of KT_CZ_Analysis.card_by_upjong.csv processing: ")
     
     if idx[1] == 1:    
@@ -38,9 +37,18 @@ if __name__ == "__main__":
         # 2. KT_CZ_Analysis.card_living_ingu.csv
         card_living_ingu = pd.read_csv("KT_CZ_Analysis.card_living_ingu.csv")
         #grouped by year,month,hd_cd,upjong_code
-        group1= card_by_upjong.groupby([card_by_upjong.year, card_by_upjong.month, card_by_upjong.hd_cd, card_by_upjong.upjong_code]) 
+        group1= card_living_ingu.groupby([card_living_ingu.year, card_living_ingu.month, card_living_ingu.hd_cd, card_living_ingu.sector_code]) 
+
+        result_2 = group1['living_ingu','revenue','customer_count','payment_count'].sum()
+
+        #end: writing CSV file    
+        result_2.to_csv('result_2.csv', header = True)
 
         print("End of KT_CZ_Analysis.card_living_ingu.csv processing: ")
+
+
+      #end: writing CSV file    
+        #result_1.to_csv('result_1.csv', header = True)
     
 
 
